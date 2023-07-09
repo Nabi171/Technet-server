d856d7bf7b9c3855f2d4505e0a03f7e7ea9f62ca
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-07-09 20:53:35 +0600

    changes in uri

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elqsdp0.mongodb.net/?retryWrites=true&w=majority`;
+const client =  new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 const run = async () => {
@@ -86,5 +88,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`techNet-backend server running on ${port}`);
 });
